26459784c84810a3654ed8087159a3181d72cabc
Michiaki <kanadebito63@yahoo.co.jp>
2016-08-04 08:08:19 +0000

    heroku test

--- app.rb
+++ app.rb
@@ -10,11 +10,3 @@ get '/hi' do
     erb :show
 end
 
-post "/hi" do
-  j = JSON.parse(request.body.string)
-  j["events"].map{ |e|
-    if e["message"]
-      "Hi, #{e["message"]["nickname"]}!"
-    end
-  }.compact.join("\n")+"\n"
-end
